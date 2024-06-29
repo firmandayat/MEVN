bd79fd7e8ebcadec8b971ce407cf9cced1d4dd82
firmandayat <firman.txt@gmail.com>
2024-06-29 20:45:43 +0700

    first

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 8000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "testing",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
